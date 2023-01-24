a338a44a570afa51819fb58cee6fc68f55dcb56d
Ryan <81717668+RyanSea@users.noreply.github.com>
2023-01-24 14:40:52 -0800

    ORACLE & MISC

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -34,15 +34,23 @@ contract Oracle {
 
     function updatePrices(address[] calldata _addresses, uint256[] calldata _values) public onlyAdmin{
 
-        require(_addresses.length == _values.length, "The length of two arrays must be same");
+        // changed: saved length to memory
+        uint length = _addresses.length;
 
-        for (uint i=0; i<_addresses.length; i++) {
+        require(length == _values.length, "UNMATCHING_ARRAYS");
+
+        // changed: made loop more gas efficient
+        
+        for (uint i=0; i < length; ) {
             prices[_addresses[i]] = _values[i];
             emit OracleUpdate(_addresses[i], _values[i], block.timestamp);
+
+            unchecked { ++i; }
         }
     }
 
-    function getTraitBooster(address _address, uint256 _id) public view returns(uint256){
+    // changed: made pure and commented out param vars to remove compiler warning
+    function getTraitBooster(address /*_address*/, uint256 /*_id*/) public pure returns(uint256){
         //random boosting for now
         return 120;
     }
@@ -52,7 +60,8 @@ contract Oracle {
 
         if (COLLECTION_DETAILS.TYPE == COLLECTION_TYPE.Oracle) {
             AggregatorV3Interface priceFeed = AggregatorV3Interface(COLLECTION_DETAILS.VAULUATION_PERFORMER);
-            (, int256 answer, , uint256 updatedAt, ) = priceFeed.latestRoundData();
+            // changed: commented out unused return item to remove compiler warning
+            (, int256 answer, , /*uint256 updatedAt*/, ) = priceFeed.latestRoundData();
 
             if (answer != 0){
                 return (uint256(answer) * getTraitBooster(_address, _id)) / 100;
@@ -63,7 +72,7 @@ contract Oracle {
            return ICustomWrapper(COLLECTION_DETAILS.VAULUATION_PERFORMER).getPrice(_address, _id);
         }
 
-        revert();
+        revert("INVALID_COLLECTION_TYPE");
     }
 }
 
